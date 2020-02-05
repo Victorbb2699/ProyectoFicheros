dff5ce9dc74ea6c3a51739ee0935fc227e8f00d9
Victor Bethencourt <victolo500@gmail.com>
2020-02-05 08:32:28 +0000

    Proyecto terminado

---
+++ Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace ProyectoFicheros
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textDirectory = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ruta actual:";
+            // 
+            // textDirectory
+            // 
+            this.textDirectory.Location = new System.Drawing.Point(80, 6);
+            this.textDirectory.Multiline = true;
+            this.textDirectory.Name = "textDirectory";
+            this.textDirectory.Size = new System.Drawing.Size(708, 20);
+            this.textDirectory.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(145, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Listar capetas y ficheros";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(15, 215);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(145, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Crear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(15, 262);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(60, 17);
+            this.radioButton1.TabIndex = 7;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Fichero";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.UseWaitCursor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(140, 262);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(62, 17);
+            this.radioButton2.TabIndex = 8;
+            this.radioButton2.Text = "Carpeta";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(166, 215);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(145, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Eliminar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(317, 215);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(145, 23);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "Renombrar o mover";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(15, 321);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(145, 23);
+            this.button5.TabIndex = 13;
+            this.button5.Text = "Visualizarfichero";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(166, 321);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(145, 23);
+            this.button6.TabIndex = 14;
+            this.button6.Text = "Modificarfichero";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 298);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 13);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Directorio o fichero";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(117, 295);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(674, 20);
+            this.textBox2.TabIndex = 16;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(15, 70);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(776, 134);
+            this.listBox1.TabIndex = 18;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(15, 350);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(773, 191);
+            this.richTextBox1.TabIndex = 19;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(798, 553);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textDirectory);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textDirectory;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ProyectoFicheros
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textDirectory.Text = Directory.GetCurrentDirectory();
+
+        }
+
+        //listar carpetas y ficheros del directorio
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+
+            String ErrorMessage = null;
+            String NewPath = " NewPath holds the path the user has entered.";
+            NewPath = textDirectory.Text;
+            try
+            {
+                Directory.SetCurrentDirectory(NewPath);
+            }
+            catch (DirectoryNotFoundException f)
+            {
+                ErrorMessage = "You must enter a valid path. If trying to access a different drive, remember to include the drive letter.";
+            }
+            catch
+            {
+                ErrorMessage = "You must enter a path.";
+            }
+            finally
+            {//' Display the error message only if one exists.
+                if(ErrorMessage != null) 
+                    MessageBox.Show(ErrorMessage);
+            }
+
+            string sourceDirectory = NewPath; 
+            var txtFiles = Directory.EnumerateFiles(sourceDirectory);
+            var dirFiles = Directory.EnumerateDirectories(sourceDirectory);
+            
+            
+            foreach (string currentFile in dirFiles)
+            {
+                string fileName = currentFile.Substring(sourceDirectory.Length + 1);
+                listBox1.Items.Add(fileName);
+            }
+
+            foreach (string currentFile in txtFiles) { 
+                string fileName = currentFile.Substring(sourceDirectory.Length + 1); 
+                listBox1.Items.Add(fileName); 
+            }
+            
+        }
+
+        //crear fichero o carpeta
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+           
+            String archivoNombre;
+            archivoNombre = textBox2.Text;
+            if (radioButton1.Checked)
+            {
+                try
+                {
+                    using (FileStream fs = File.Create(archivoNombre))
+                    {
+                        byte[] info = new UTF8Encoding(true).GetBytes("");
+                        // Add some information to the file.
+                        fs.Write(info, 0, info.Length);
+                    }
+
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+            else if(radioButton2.Checked)
+            {
+                DirectoryInfo di = Directory.CreateDirectory(archivoNombre);
+            }
+
+        }
+
+        //eliminar 
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+
+            try
+            {
+                String path = textDirectory.Text;
+                String path2 = textBox2.Text;
+
+                if (radioButton2.Checked)
+                {
+                    if (Directory.Exists(path2))
+                    {
+                        Directory.Delete(path2, true);
+                    }
+
+
+                }
+                else if (radioButton1.Checked)
+                {
+
+                    if (File.Exists(path2))
+                    {
+                        File.Delete(path2);
+                    }
+
+                }
+            }
+            catch (Exception)
+            {
+               
+            }
+
+        }
+        //renombrar o mover
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+
+
+            try
+            {
+                String path = textDirectory.Text;
+                String path2 = textBox2.Text;
+
+
+                if (radioButton2.Checked)
+                {
+                    if (!Directory.Exists(path2))
+                    {
+                        Directory.Move(path, path2);
+                    }
+
+
+                }
+                else if (radioButton1.Checked)
+                {
+
+                    if (!File.Exists(path2))
+                    {
+                        File.Move(path, path2);
+                    }
+
+                }
+            }
+            catch (Exception)
+            {
+              
+            }
+
+
+
+
+
+        }
+        //visualizar fichero
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            String line;
+            String name = listBox1.SelectedItem.ToString();
+
+            richTextBox1.Text = File.ReadAllText(name);
+        }
+
+        //modfich
+        private void button6_Click(object sender, EventArgs e)
+        {
+            File.WriteAllText(listBox1.SelectedItem.ToString(), richTextBox1.Text);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
